d9e6ed5c1e9cb3f32e87838a5267dee11167e9b3
kenkay <kaloohken@gmail.com>
2018-05-17 16:25:08 +0300

    Added combined search for both series and movies items in the database

--- article.php
+++ article.php
@@ -9,7 +9,7 @@
   <?php
       $title = mysqli_real_escape_string($conn, $_GET['title']);
 
-      $sql = "SELECT * FROM Dreamz WHERE MovieName ='$title'";
+      $sql = "SELECT * FROM DreamzS WHERE MovieName ='$title'";
       $result = mysqli_query($conn, $sql);
       $queryResults = mysqli_num_rows($result);
 
@@ -17,11 +17,28 @@
           while($row = mysqli_fetch_assoc($result)) {
             echo "<div class='article-box'>
                 <h3>".$row['MovieName']."</h3>
-                <h3>".$row['Episodes']."</h3>
+                <h3>".$row['Rating']."</h3>
              </div>";
           }
       }
    ?>
+
+   <?php
+       $title = mysqli_real_escape_string($conn, $_GET['title']);
+
+       $sql = "SELECT * FROM Dreamz WHERE MovieName ='$title'";
+       $result = mysqli_query($conn, $sql);
+       $queryResults = mysqli_num_rows($result);
+
+       if ($queryResults > 0) {
+           while($row = mysqli_fetch_assoc($result)) {
+             echo "<div class='article-box'>
+                 <h3>".$row['MovieName']."</h3>
+                 <h3>".$row['Episodes']."</h3>
+              </div>";
+           }
+       }
+    ?>
 
 </body>
 
--- search.php
+++ search.php
@@ -9,7 +9,7 @@
 <?php
     if (isset($_POST['submit-search'])) {
       $search = mysqli_real_escape_string($conn, $_POST['search']);
-      $sql = "SELECT * FROM Dreamz WHERE MovieName LIKE '%$search%' OR Episodes LIKE '%$search%'";
+      $sql = "SELECT * FROM DreamzS WHERE MovieName LIKE '%$search%' OR Rating LIKE '%$search%'";
       $result = mysqli_query($conn, $sql);
       $queryResult = mysqli_num_rows($result);
 
@@ -19,7 +19,7 @@
         while ($row = mysqli_fetch_assoc($result)) {
           echo "<a href='article.php?title=".$row['MovieName']."' <div class='article-box'>
               <h3>".$row['MovieName']."</h3>
-              <h3>".$row['Episodes']."</h3>
+              <h3>".$row['Rating']."</h3>
            </div></a>";
         }
       } else {
@@ -29,4 +29,28 @@
 
 
  ?>
+
+ <?php
+     if (isset($_POST['submit-search'])) {
+       $search = mysqli_real_escape_string($conn, $_POST['search']);
+       $sql = "SELECT * FROM Dreamz WHERE MovieName LIKE '%$search%' OR Episodes LIKE '%$search%'";
+       $result = mysqli_query($conn, $sql);
+       $queryResult = mysqli_num_rows($result);
+
+       //echo "There are ".$queryResult. " results! <br>";
+
+       if ($queryResult > 0) {
+         while ($row = mysqli_fetch_assoc($result)) {
+           echo "<a href='article.php?title=".$row['MovieName']."' <div class='article-box'>
+               <h3>".$row['MovieName']."</h3>
+               <h3>".$row['Episodes']."</h3>
+            </div></a>";
+         }
+       } else {
+         echo "There are no results";
+       }
+     }
+
+
+  ?>
 </div>
